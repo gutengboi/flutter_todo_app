b5fe19a0b7b6b3ad17d55f5cdcacdffacda763a2
gutengboi <animashaunolayinka195@gmail.com>
2024-11-25 22:55:05 +0100

    testing testing

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo_app/ui/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Todo App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+      ),
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/ui/home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Text("Hello World",
+          style: TextStyle(
+            fontSize: 30
+          ),),
+        ],
+      ),
+    );
+  }
+}
